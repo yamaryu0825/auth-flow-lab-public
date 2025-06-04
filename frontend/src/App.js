a3835d1ceabca4ab5c24336f739afa587aba5d64
Ryuichiro Yamamoto <74702717+yamaryu0825@users.noreply.github.com>
2025-06-05 08:08:47 +0900

    React フロントエンドを追加

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>OAuth / SAML / 証明書 認証ツール</h1>
+      <p>ここに認証フローを検証できるUIを作っていきます。</p>
+    </div>
+  );
+}
+
+
+export default App;
